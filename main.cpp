9606a8cd8d24096f9b0cd2304f83d4c07bf1ae8f
kystyn <konstantindamaskinskiy@gmail.com>
2019-09-05 00:09:47 +0300

    student correction

--- main.cpp
+++ main.cpp
@@ -7,37 +7,14 @@ int main( int argc, char *argv[] )
     // toolkit near test images!
     try
     {
-        if (argc != 7)
+        if (argc != 3)
             throw "Not enough arguments";
 
         png_toolkit testTool;
-        testTool.load(argv[2]);
-        if (png_toolkit::filters.find(argv[1]) != png_toolkit::filters.end())
-            testTool.applyFilter(png_toolkit::filters[argv[1]]);
-        else
-            throw "Bad filter";
+        studTool.load(argv[1]);
+	studTool.fillHalfRed();
+	studTool.save(argv[2]);
 
-        png_toolkit studTool;
-        system(("./" + std::string(argv[3]) + ' ' +  argv[2] + ' ' + argv[4]).c_str());
-        studTool.load(argv[4]);
-
-        png_toolkit::Error err;
-        int diffPix;
-        auto mse = testTool.mseDeviation(studTool, err, diffPix);
-        if (err == png_toolkit::Error::Ok) // TODO num of diff pixels limits
-        {
-            if (diffPix < std::stoi(argv[5]))
-                std::cout << "OK ";
-            else
-                std::cout << "Too many bad pixels: " << diffPix << " ";
-
-            if (mse < std::stoi(argv[6]))
-                std::cout << "OK";
-            else
-                std::cout << "Too big MSE: " << mse;
-        }
-        else
-            throw "Bad picture size/format";
     }
     catch (const char *str)
     {

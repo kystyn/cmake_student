e22a8b1cb12440cf6789c28168a3825ba39ea5ed
kystyn <konstantindamaskinskiy@gmail.com>
2019-10-24 23:27:56 +0300

    repair threshold

--- threshold.h
+++ threshold.h
@@ -26,12 +26,11 @@ public:
                 throw "No memory";
 
             memcpy(saved, imgData.pixels, imgData.w * imgData.h * cpp);
-            ///preprocess({saved, imgData.w, imgData.h, imgData.compPerPixel});
-            preprocess(imgData);
+            preprocess({saved, imgData.w, imgData.h, imgData.compPerPixel});
 
             // pixel - center pixel
             auto applyMedian =
-                    [&imgData, &ar, cpp, &indices, saved, x0, y0]( int pixelX, int pixelY, int channel ) -> void
+                    [&imgData, &ar, cpp, &indices, saved, x0, y0]( int pixelX, int pixelY ) -> void
             {
                 int
                     xStart = std::max(x0, pixelX - matrixSize / 2),
@@ -42,7 +41,7 @@ public:
                 int i = 0;
                 for (int y = yStart; y <= yEnd; y++)
                     for (int x = xStart; x <= xEnd; x++)
-                        indices[i++] = (y * imgData.w + x) * cpp + channel;
+                        indices[i++] = (y * imgData.w + x) * cpp;
 
                 std::sort(indices.begin(),
                           indices.begin() + (yEnd - yStart + 1) * (xEnd - xStart + 1),
@@ -52,17 +51,25 @@ public:
                             });
 
                 for (int i = 0; i < (yEnd - yStart + 1) * (xEnd - xStart + 1) / 2; i++)
-                    imgData.pixels[indices[i]] = 0;
+                {
+                    imgData.pixels[indices[i] + 0] = 0;
+                    imgData.pixels[indices[i] + 1] = 0;
+                    imgData.pixels[indices[i] + 2] = 0;
+                }
+                for (int i = (yEnd - yStart + 1) * (xEnd - xStart + 1) / 2;
+                     i < (yEnd - yStart + 1) * (xEnd - xStart + 1); i++)
+                {
+                    imgData.pixels[indices[i] + 0] = saved[indices[i] + 0];
+                    imgData.pixels[indices[i] + 1] = saved[indices[i] + 1];
+                    imgData.pixels[indices[i] + 2] = saved[indices[i] + 2];
+                }
             };
 
-
-            ///
-             /*for (; y < imgData.h / ar.bottom; y++) {
+            for (; y < imgData.h / ar.bottom; y++) {
                 x = ar.left == 0 ? 0 : imgData.w / ar.left;
                 for (; x < imgData.w / ar.right; x++)
-                    for (int c = 0; c < 3; c++)
-                        applyMedian(x, y, c);
-            }*/
+                    applyMedian(x, y);
+            }
 
             delete []saved;
         }

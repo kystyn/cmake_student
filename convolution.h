fdb82f387612ad5fda8cc919c5911e779aad5795
kystyn <konstantindamaskinskit@gmail.com>
2019-10-24 15:53:03 +0300

    Convoutuional filters implemented

--- convolution.h
+++ convolution.h
@@ -2,56 +2,67 @@
 #define CONVOLUTION_H
 
 #include <array>
+#include <cstring>
 #include "base_filter.h"
 
 namespace filter {
+
 template<int N>
 class convolution : public base {
 public:
     convolution( std::string const &filterName, std::array<std::array<double, N>, N> const &ker ) :
         base(filterName), ker(ker) {}
 
-    void operator()( image_data &imgData, area &ar ) override
-    {/*
-        auto convolute =
-                [&imgData, cpp, &indices, matrixSize]( int pixelX, int pixelY ) -> void
-        {
-            int
-                xStart = std::max(0, pixelX - matrixSize / 2),
-                yStart = std::max(0, pixelY - matrixSize / 2),
-                xEnd = std::min(imgData.w - 1, pixelX + matrixSize / 2),
-                yEnd = std::min(imgData.h - 1, pixelY + matrixSize / 2);
-
-            int i = 0;
-            for (int y = yStart; y <= yEnd; y++)
-                for (int x = xStart; x <= xEnd; x++)
-                    indices[i++] = (y * imgData.w + x) * cpp  + channel;
-
-            std::sort(indices.begin(),
-                      indices.begin() + (yEnd - yStart + 1) * (xEnd - xStart + 1),
-                        [&imgData]( int idx1, int idx2 ) -> bool
-                        {
-                            return imgData.pixels[idx1] < imgData.pixels[idx2];
-                        });
-
-            for (int i = 0; i < (yEnd - yStart + 1) * (xEnd - xStart + 1) / 2; i++)
-                imgData.pixels[indices[i]] = 0;
-        };
-
+    void operator()( image_data &imgData, area const &ar ) override
+    {
         auto cpp = imgData.compPerPixel;
         if (cpp == 4 || cpp == 3) {
+            const int matrixSize = 3;
+            int x = ar.left == 0 ? 0 : imgData.w / ar.left;
             int y = ar.top == 0 ? 0 : imgData.h / ar.top;
-            for (; y < imgData.h / ar.bottom; y++) {
-                int x = ar.left == 0 ? 0 : imgData.w / ar.left;
+
+            stbi_uc *saved = new stbi_uc[imgData.h * imgData.w * cpp];
+            if (saved == nullptr)
+                throw "No memory";
+
+            memcpy(saved, imgData.pixels, imgData.w * imgData.h * cpp);
+
+            double norm = 0;
+
+            for (auto y : ker)
+                for (auto x : y)
+                    norm += x;
+
+            auto convolute =
+                [&imgData, cpp, matrixSize, saved, norm]( int pixelX, int pixelY, stbi_uc *resC ) -> void
+            {
+                int
+                    xStart = std::max(0, pixelX - matrixSize / 2),
+                    yStart = std::max(0, pixelY - matrixSize / 2),
+                    xEnd = std::min(imgData.w - 1, pixelX + matrixSize / 2),
+                    yEnd = std::min(imgData.h - 1, pixelY + matrixSize / 2);
+
+                double newC[3] = {0};
+                for (int y = -N / 2; y < N / 2; y++)
+                    for (int x = -N / 2; x < N / 2; x++)
+                        if (pixelY + y >= yStart && pixelY + y <= yEnd &&
+                            pixelX + x >= xStart && pixelX + x <= xEnd)
+                            for (int c = 0; c < 3; c++)
+                                newC[c] += saved[((pixelY + y) * imgData.w + pixelX + x) * cpp + c] / norm;
+
+
+                resC[0] = stbi_uc(newC[0]);
+                resC[1] = stbi_uc(newC[1]);
+                resC[2] = stbi_uc(newC[2]);
+            };
+
+            for (; y < imgData.h / ar.bottom; y++)
+            {
+                x = ar.left == 0 ? 0 : imgData.w / ar.left;
                 for (; x < imgData.w / ar.right; x++)
-                {
-                    imgData.pixels[(y * imgData.w + x) * cpp + 0] = 255;
-                    imgData.pixels[(y * imgData.w + x) * cpp + 1] = 0;
-                    imgData.pixels[(y * imgData.w + x) * cpp + 2] = 0;
-                    imgData.pixels[(y * imgData.w + x) * cpp + 3] = 255;
-                }
+                    convolute(x, y, imgData.pixels + (y * imgData.w + x) * cpp);
             }
-        }*/
+        }
     }
 
 private:
